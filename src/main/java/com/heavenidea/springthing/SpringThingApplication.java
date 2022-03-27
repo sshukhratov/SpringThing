734cc5e1973c0750a47f4afa0e3cae0ff4d1060f
sshukhratov <sshukhratov@gmail.com>
2022-03-26 21:30:41 -0500

    initial commit

---
+++ src/main/java/com/heavenidea/springthing/SpringThingApplication.java
@@ -0,0 +1,13 @@
+package com.heavenidea.springthing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringThingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringThingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/heavenidea/springthing/SpringThingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heavenidea.springthing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringThingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
